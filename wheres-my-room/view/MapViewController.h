9e009f7eb595ecd2f3e603bd7bdd5a4b7655a02e
Chu-An Hsieh <cahsieh@yahoo-inc.com>
2015-07-15 09:18:32 +0800

    Initial commit.

---
+++ wheres-my-room/view/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  wheres-my-room
+//
+//  Created by Chu-An Hsieh on 7/15/15.
+//  Copyright (c) 2015 Chu-An Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
---
+++ wheres-my-roomTests/wheres_my_roomTests.m
@@ -0,0 +1,40 @@
+//
+//  wheres_my_roomTests.m
+//  wheres-my-roomTests
+//
+//  Created by Chu-An Hsieh on 7/15/15.
+//  Copyright (c) 2015 Chu-An Hsieh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface wheres_my_roomTests : XCTestCase
+
+@end
+
+@implementation wheres_my_roomTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
